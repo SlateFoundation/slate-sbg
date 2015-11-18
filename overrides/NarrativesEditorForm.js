823ade501d4dc095bb9bddd0f422695f9c68cfd9
Chris Alfano <chris@jarv.us>
2015-11-18 09:10:43 -0500

    Remove cruft

--- overrides/NarrativesEditorForm.js
+++ overrides/NarrativesEditorForm.js
@@ -8,16 +8,13 @@ Ext.define('Slate.sbg.overrides.NarrativesEditorForm', {
     initComponent: function() {
         var me = this;
 
-        me.items = Ext.Array.insert(Ext.Array.clone(me.items), 1,[{
+        me.items = Ext.Array.insert(Ext.Array.clone(me.items), 1, [{
             itemId: 'sbgPromptsFieldset',
 
             xtype: 'fieldset',
             title: 'Standards-based grading worksheet',
             defaultType: 'sbg-worksheets-prompt',
             hidden: true
-            // collapsible: true,
-            // stateful: true,
-            // stateId: 'sbg-prompts-fieldset'
         }]);
 
         me.callParent(arguments);
