3f5a0d3cbf4a0df003c5865c2e81faa9c497bc25
Chris Alfano <chris@jarv.us>
2015-11-20 12:32:01 -0500

    Tweak prompt combo width and disable matchFieldWidth

--- sencha-workspace/packages/slate-sbg/src/sbg/widget/WorksheetPrompt.js
+++ sencha-workspace/packages/slate-sbg/src/sbg/widget/WorksheetPrompt.js
@@ -15,13 +15,14 @@ Ext.define('Slate.sbg.widget.WorksheetPrompt', {
     items: [
         {
             xtype: 'combobox',
-            width: 75,
+            width: 60,
 
             submitValue: false,
             store: 'StandardsWorksheetPromptOptions',
             queryMode: 'local',
             forceSelection: true,
-            autoSelect: true
+            autoSelect: true,
+            matchFieldWidth: false
         },
         {
             itemId: 'promptCmp',
