e82ea6e9b4e83bccb650fb46542541c5e3f7ed4c
Chris Alfano <chris@jarv.us>
2015-11-20 12:32:31 -0500

    Wrap prompts store options in config object

--- sencha-workspace/packages/slate-sbg/src/sbg/store/StandardsWorksheetPromptOptions.js
+++ sencha-workspace/packages/slate-sbg/src/sbg/store/StandardsWorksheetPromptOptions.js
@@ -2,12 +2,14 @@
 Ext.define('Slate.sbg.store.StandardsWorksheetPromptOptions', {
     extend: 'Ext.data.Store',
 
-    fields: ['id', 'text'],
-    data: [
-        { id: 0, text: 'N/A' },
-        { id: 1, text: '1' },
-        { id: 2, text: '2' },
-        { id: 3, text: '3' },
-        { id: 4, text: '4' }
-    ]
+    config: {
+        fields: ['id', 'text'],
+        data: [
+            { id: 0, text: 'N/A' },
+            { id: 1, text: '1' },
+            { id: 2, text: '2' },
+            { id: 3, text: '3' },
+            { id: 4, text: '4' }
+        ]
+    }
 });
