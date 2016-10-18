4bf1ac77a8c91ab576528a2a61d1e1baf40ccd5f
Chris Alfano <chris@jarv.us>
2016-10-18 12:33:35 -0400

    Remove core progress  links from config

--- php-config/Slate/UI/Tools.config.d/sbg.php
+++ php-config/Slate/UI/Tools.config.d/sbg.php
@@ -4,25 +4,7 @@ if (empty($GLOBALS['Session']) || !$GLOBALS['Session']->hasAccountLevel('Staff')
     return;
 }
 
-Slate\UI\Tools::$tools['Standards-Based Grading'] = [
-    'Narratives' => [
-        '_href' => '/manage#progress/narratives',
-        '_icon' => 'documents'
-    ],
-    'Standards' => [
-        '_href' => '/manage#progress/standards',
-        '_icon' => 'clipboard'
-    ],
-    'Interims' => [
-        '_href' => '/manage#progress/interims',
-        '_icon' => 'interims'
-    ],
-    'Progress Notes' => [
-        '_href' => '/manage#progress/notes',
-        '_icon' => 'notes'
-    ],
-    'Growth Dashboard' => [
-        '_href' => '/sbg/teachers',
-        '_icon' => 'area-chart'
-    ]
+Slate\UI\Tools::$tools['Standards-Based Grading']['Growth Dashboard'] = [
+    '_href' => '/sbg/teachers',
+    '_icon' => 'area-chart'
 ];
