f28cfe86353f2da11fed1e75898653140e08b0f6
Nafis Bey <nafis@jarv.us>
2016-12-20 19:31:56 -0500

    Move git php-config file to top of tree list

--- php-config/Git.config.d/slate-sbg.php
+++ php-config/Git.config.d/slate-sbg.php
@@ -5,11 +5,11 @@ Git::$repositories['slate-sbg'] = [
     'originBranch' => 'builds/v1',
     'workingBranch' => 'builds/v1',
     'trees' => [
+        'php-config/Git.config.d/slate-sbg.php',
         'html-templates/sbg',
         'html-templates/sbg/progress/section-term-reports/sbg-print.tpl',
         'html-templates/subtemplates/sbg.tpl',
         'php-classes/Slate/SBG',
-        'php-config/Git.config.d/slate-sbg.php',
         'php-config/Slate/Progress/SectionTermReport.config.d/sbg.php',
         'php-config/Slate/Progress/SectionTermReportsRequestHandler.config.d/print-template.php',
         'php-config/Slate/UI/Tools.config.d/sbg.php',
