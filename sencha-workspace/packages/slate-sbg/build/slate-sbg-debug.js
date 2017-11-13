d1d1f99b2b18c3300be75a7b3a31e3118237e321
Chris Alfano <chris@jarv.us>
2017-11-13 13:18:51 -0500

    Add build for v1.3.4

--- sencha-workspace/packages/slate-sbg/build/slate-sbg-debug.js
+++ sencha-workspace/packages/slate-sbg/build/slate-sbg-debug.js
@@ -2074,7 +2074,7 @@ Ext.define('Slate.sbg.overrides.SlateAdmin', {override:'SlateAdmin.Application',
 }});
 Ext.define('Slate.sbg.overrides.TermReportSectionsGrid', {override:'SlateAdmin.view.progress.terms.SectionsGrid', requires:['Ext.grid.plugin.CellEditing', 'Ext.form.field.ComboBox'], width:300, worksheetColumnTitle:'Worksheet', emptyWorksheetText:'\x3cem\x3eDouble-click to select\x3c/em\x3e', initComponent:function() {
   var me = this;
-  me.columns = me.columns.concat({text:me.worksheetColumnTitle, dataIndex:'WorksheetID', emptyCellText:me.emptyWorksheetText, width:200, editor:{xtype:'combo', allowBlank:true, emptyText:'Select worksheet', store:'StandardsWorksheets', displayField:'Title', valueField:'ID', queryMode:'local', triggerAction:'all', typeAhead:true, forceSelection:true, selectOnFocus:true}, renderer:function(worksheetId, metaData, section) {
+  me.columns = me.columns.concat({flex:1, text:me.worksheetColumnTitle, dataIndex:'WorksheetID', emptyCellText:me.emptyWorksheetText, editor:{xtype:'combo', allowBlank:true, emptyText:'Select worksheet', matchFieldWidth:false, store:'StandardsWorksheets', displayField:'Title', valueField:'ID', queryMode:'local', triggerAction:'all', typeAhead:true, forceSelection:true, selectOnFocus:true}, renderer:function(worksheetId, metaData, section) {
     if (!worksheetId) {
       return null;
     }
